7892f1ec93ca86f4b3da376560cc5a4b6f8ccd9c
Ramyasree3010 <39622426+Ramyasree3010@users.noreply.github.com>
2018-06-06 16:07:01 +0530

    Create factorial.py
    sample

---
+++ factorial.py
@@ -0,0 +1,21 @@
+def recur_factorial(n):
+   
+   
+   if n == 1:
+       return n
+   else:
+       return n*recur_factorial(n-1)
+
+
+num=int(input())
+
+
+
+
+
+if num < 0:
+   print("invaild data")
+elif num == 0:
+   print(1)
+else:
+   print(recur_factorial(num))
